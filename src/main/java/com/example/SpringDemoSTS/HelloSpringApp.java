05499adac060a49fdf4df3a77e66f7f84f761bfb
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-02-22 16:03:37 +0530

    [Pranjal_Haswani]add. MVC Controller Application.

--- src/main/java/com/example/SpringDemoSTS/HelloSpringApp.java
+++ src/main/java/com/example/SpringDemoSTS/HelloSpringApp.java
@@ -1,5 +1,8 @@
 package com.example.SpringDemoSTS;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloSpringApp {
 
     @GetMapping("/hello")
-    public String helloWorldUC1(){
+    public String helloWorldUC1() {
+        return "Hello from BridgeLabz";
+    }
+
+    //  UC2: MVC with Thymeleaf - Returns a web page
+    @GetMapping("/web-hello")
+    public String displayMessageUC2(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
         return "Hello from BridgeLabz";
     }
 }
