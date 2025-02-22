bbfdc92912b5d043e51b4c25cd1dc278af4367d1
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-02-22 11:54:07 +0530

    [Pranjal_Haswani]add. Hello from BridgeLabz.

---
+++ src/main/java/com/example/SpringDemoSTS/SpringDemoStsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDemoSTS/SpringDemoStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDemoSTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
